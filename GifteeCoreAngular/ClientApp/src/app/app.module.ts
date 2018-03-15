624f020a1e37de790b558d36cc79afce0e2613f8
Gabor Koncz <gabee1987@gmail.com>
2018-03-15 15:03:37 +0100

    added ng2-toasty, tested with counter component, working

--- GifteeCoreAngular/ClientApp/src/app/app.module.ts
+++ GifteeCoreAngular/ClientApp/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+
+// External Angular modules components
+import { ToastyModule } from 'ng2-toasty';
 
 @NgModule({
   declarations: [
@@ -22,6 +27,7 @@ import { FetchDataComponent } from './components/fetch-data/fetch-data.component
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ToastyModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- GifteeCoreAngular/ClientApp/src/app/components/counter/counter.component.ts
+++ GifteeCoreAngular/ClientApp/src/app/components/counter/counter.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
 
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
+  constructor(private toastyService: ToastyService) {
+  }
+
   public currentCount = 0;
 
   public incrementCounter() {
     this.currentCount++;
+  }
+
+  addToast() {
+    // Create the instance of ToastOptions 
+    var successType: ToastOptions = {
+      title: "Counter increased by",
+      msg: "1",
+      showClose: true,
+      timeout: 5000,
+      theme: 'bootstrap',
+      onAdd: (toast: ToastData) => {
+        console.log('Toast ' + toast.id + ' has been added!');
+      },
+      onRemove: function (toast: ToastData) {
+        console.log('Toast ' + toast.id + ' has been removed!');
+      }
+    };
+
+
+    this.toastyService.success(successType);
+
   }
 }
