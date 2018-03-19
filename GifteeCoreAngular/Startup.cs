0ab2c41893c661fd3b4f3c27e388ded116f761ce
Gabor Koncz <gabee1987@gmail.com>
2018-03-19 10:59:35 +0100

    temporarly removed https connect

--- GifteeCoreAngular/Startup.cs
+++ GifteeCoreAngular/Startup.cs
@@ -57,7 +57,7 @@ namespace GifteeCoreAngular
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
